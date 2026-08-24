6b7b9e4ef3e5dda9ae7771956d9265d7abc2be17
Udeh-png <leonwokedichisom@gmail.com>
2026-08-24 22:47:55 +0100

    Create Spring Boot project

---
+++ fintech-auth-backend/src/main/java/com/fintechauth/fintech_auth_backend/FintechAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fintechauth.fintech_auth_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechAuthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechAuthBackendApplication.class, args);
+	}
+
+}
---
+++ fintech-auth-backend/src/test/java/com/fintechauth/fintech_auth_backend/FintechAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintechauth.fintech_auth_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechAuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
