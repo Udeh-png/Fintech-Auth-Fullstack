450e94eaa37fc074ba52cbab7a40ba5fecf0f4ad
Udeh-png <leonwokedichisom@gmail.com>
2026-08-28 07:12:26 +0100

    feat: add application.properties for configuration settings and exclude DataRedisRepositoriesAutoConfiguration

--- fintech-auth-backend/src/main/java/com/fintechauth/fintech_auth_backend/FintechAuthBackendApplication.java
+++ fintech-auth-backend/src/main/java/com/fintechauth/fintech_auth_backend/FintechAuthBackendApplication.java
@@ -2,8 +2,9 @@ package com.fintechauth.fintech_auth_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataRedisRepositoriesAutoConfiguration.class })
 public class FintechAuthBackendApplication {
 
 	public static void main(String[] args) {
